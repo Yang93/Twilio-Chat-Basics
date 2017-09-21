8f16d3f477bf2c101f506cbd3247fb4a793dee11
Mengyang Hong <mengyang.hong@gmail.com>
2017-09-21 09:56:24 -0400

    ignore .env files

--- app.rb
+++ app.rb
@@ -7,3 +7,28 @@ configure :development do
   require 'dotenv'
   Dotenv.load
 end
+
+get '/sms/incoming' do
+  
+end
+
+enable :sessions
+
+# create a twilio client using your account info
+@client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+
+# create an endpoint to handle incoming requests from Twilio
+get "/sms/incoming" do
+  session["counter"] ||= 1
+  body = params[:Body] || “"
+
+  twiml = Twilio::TwiML::MessagingResponse.new do |r|
+    r.message do |m|
+      m.body( "You said: " + body + “\n It’s message number #{ session["counter"] }” )
+   end
+  end
+  session["counter"] += 1
+  content_type 'text/xml'
+  twiml.to_s
+
+end
